2271a310317e573ade68dba71e54fd1943815e3e
mathildac <39262022+mathildac@users.noreply.github.com>
2018-05-22 11:37:29 +0200

    Add files via upload

---
+++ Projektarbete/firstpage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Studentchatten </title>
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+  </head>
+  <body>
+    <ul>
+      <li><a class="active" href="firstpage.php">Hem</a></li>
+      <li><a href="profil.php">Profil</a></li>
+      <li><a href="rewards.php">Belöningar</a></li>
+    </ul>
+    <div class="searchbardiv">
+      <form action="">
+        <input type="text" placeholder="Sök på en kurs..." id="searchbar">
+        <button type="submit">
+          <!-- <i class="fa fa-search" aria-hidden="true"></i> kod för sök ikon, men förstår ej hur det fungerar... https://fontawesome.com/v4.7.0/get-started/-->
+        </button>
+      </form>
+    </div>
+  </body>
+</html>
--- Projektarbete/index.php
+++ Projektarbete/index.php
@@ -2,18 +2,13 @@
 <html>
   <head>
     <title> Studentchatten </title>
-    <link rel="stylesheet" href="assets/css/main.css" type="text/css" />
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
   </head>
   <body>
-    <h1>Logga in </h1>
-  <div class='login'>
-    <form name="login" method="POST" onsubmit="" action="">
-        <label for="username"> Personnummer eller e-postadress </label><br/>
-        <input type="text" id="username" name="username"><br/>
-        <label for="password"> Lösenord </label><br/>
-        <input type="password" id="password" name="password"><br/><br/>
-        <input type="submit" value="Logga in" id="skicka">
-  </div>
-    </form>
+    <div class='loginlink'>
+      <form name="loginbutton" method="POST" onsubmit="" action="login.php">
+          <input type="submit" value="Logga in" id="loginbutton">
+      </form>
+    </div>
   </body>
 </html>
