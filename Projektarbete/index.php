db68ed78dc1e2cc17d757641f6ecbd52e78c8c3c
mathildac <39262022+mathildac@users.noreply.github.com>
2018-05-22 09:41:43 +0200

    Add files via upload

---
+++ Projektarbete/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Studentchatten </title>
+    <link rel="stylesheet" href="assets/css/main.css" type="text/css" />
+  </head>
+  <body>
+    <h1>Logga in </h1>
+  <div class='login'>
+    <form name="login" method="POST" onsubmit="" action="">
+        <label for="username"> Personnummer eller e-postadress </label><br/>
+        <input type="text" id="username" name="username"><br/>
+        <label for="password"> Lösenord </label><br/>
+        <input type="password" id="password" name="password"><br/><br/>
+        <input type="submit" value="Logga in" id="skicka">
+  </div>
+    </form>
+  </body>
+</html>
