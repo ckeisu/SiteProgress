ea537ef4aa2882ef0cd33ce73aadedf45a86c537
mathildac <39262022+mathildac@users.noreply.github.com>
2018-05-24 12:39:08 +0200

    Add files via upload

--- Projektarbete/firstpage.php
+++ Projektarbete/firstpage.php
@@ -1,23 +1,25 @@
 <?php
-//startar en session.
-session_start();
-// Kolla om användaren är inloggad, och då får denna tillgång till index.php, säkerhetskoll
-if(!isset($_SESSION['loggedin']))
-{
- header('Location:index.php');
-}
+  session_start();
+  if(!isset($_SESSION['inlogged'])){
+    header("Location:Login.php");
+  }
+  else
+  {
+    $username = $_SESSION['username'];
+  }
+
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <title> Studentchatten </title>
     <link rel="stylesheet" href="css/main.css" type="text/css" />
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
   </head>
   <body>
     <div class="header">
-      <h1> Pluggkollen </h1>
+      <h1> Pluggkollen <?php echo $username  ?></h1>
     <ul>
       <li><a class="active" href="firstpage.php">Hem</a></li>
       <li><a href="profil.php">Profil</a></li>
@@ -30,8 +32,7 @@ if(!isset($_SESSION['loggedin']))
       <form name=""action="">
         <h2> Sök till en kurs eller ett program här </h2>
         <input type="text" placeholder="Sök på en kurs..." id="searchbar">
-        <button type="submit"> Sök
-          <!-- <i class="fa fa-search" aria-hidden="true"></i> kod för sök ikon, men förstår ej hur det fungerar... https://fontawesome.com/v4.7.0/get-started/-->
+        <button type="submit"><i class="fa fa-search" aria-hidden="true"></i> 
         </button>
       </div>
       </form>
--- Projektarbete/loginprocess.php
+++ Projektarbete/loginprocess.php
@@ -5,7 +5,7 @@ $username = mysqli_real_escape_string($conn, $_POST['username']);
 $password = mysqli_real_escape_string($conn, $_POST['password']);
 
 //Query för att kolla om så att personen skriver in rätt epost och lösenord
-$sql = "SELECT * FROM user WHERE epost = '$username’ OR personnummer = ’$username' AND password = '$password'";
+$sql = "SELECT * FROM user WHERE (epost = '$username' OR personnummer = '$username') AND password = '$password'";
 $resultat = mysqli_query($conn, $sql)or die("Bad SQL: $sql");
 
 
@@ -13,7 +13,7 @@ if(mysqli_num_rows($resultat) > 0){
   echo "Success";
   SESSION_START();
   $_SESSION['inlogged'] = true;
-  $_SESSION[’username’] = $username;
+  $_SESSION['username'] = $username;
   header('Location: firstpage.php');
   exit();
 
