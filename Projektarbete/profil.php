5155dd57b24c60ba10e4fe10f0837bad1029c768
mathildac <39262022+mathildac@users.noreply.github.com>
2018-05-25 22:22:39 +0200

    Add files via upload

--- Projektarbete/profil.php
+++ Projektarbete/profil.php
@@ -1,10 +1,12 @@
 <?php
-//startar en session.
-session_start();
 // Kolla om användaren är inloggad, och då får denna tillgång till index.php, säkerhetskoll
-if(!isset($_SESSION['loggedin']))
+session_start();
+if(!isset($_SESSION['inlogged'])){
+  header("Location:login.php");
+}
+else
 {
- header('Location:loggain.php');
+  $username = $_SESSION['username'];
 }
 
 ?>
@@ -25,20 +27,46 @@ if(!isset($_SESSION['loggedin']))
       <li> <a href="message.php">Meddelande</a></li>
     </ul>
   </div>
+    <?php
+    include 'connect.php';
 
-  <div class="about">
+      $profilquery = $conn->query("SELECT DISTINCT namn, epost, u.personnummer, anvtyp FROM kurs k
+      JOIN anv_kurs ak
+      ON k.kurskod = ak.kurskod
+      JOIN user u
+      ON ak.personnummer = u.personnummer
+      WHERE namn = '$username' OR u.personnummer = '$username';");
 
+          while($row = $profilquery->fetch_assoc())
+          {
+            $namn = $row['namn'];
+            $epost = $row['epost'];
+            $personnummer = $row['personnummer'];
+            $anvtyp = $row['anvtyp'];
 
-    <h2> About </h2>
-    <p> Namn: </p>
-    <br>
-    <p> Epost: </p>
+            echo "<div class = 'about'> <h2> Din profil </h2> Namn: $namn </br> E-post: $epost </br> Personnummer: $personnummer </br> Konto: $anvtyp </br>
+            </div>";
+          }
 
+      $profilkursquery = $conn->query("SELECT universitet, kursnamn, datum FROM kurs k
+      JOIN anv_kurs ak
+      ON k.kurskod = ak.kurskod
+      JOIN user u
+      ON ak.personnummer = u.personnummer
+      WHERE namn = '$username' OR u.personnummer = '$username';
+      ");
+      echo '<h2> Slutförda kurser </h2>';
+      while($row = $profilkursquery->fetch_assoc())
+      {
+        $universitet = $row['universitet'];
+        $kursnamn = $row['kursnamn'];
+        $datum = $row['datum'];
 
-
-    </div>
-
-    <div class="footer">
-    </div>
+        echo "<div class = 'about'>  Kursnamn: $kursnamn </br> Universitet: $universitet </br> Slutförd: $datum </br>
+        </div>";
+      }
+     ?>
+    <!-- <div class="footer">
+    </div> -->
   </body>
 </html>
